d6753397803745594d8e41f3ac3a6df7d966dcc0
Masterfoni <joaovitor8891879@gmail.com>
2020-01-18 17:28:00 -0300

    Removing unused files and adding first form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="app">
+        <aside>
+          <strong>Cadastrar</strong>
+          <form>
+            <div className="input-block">
+              <label htmlFor="github_username">Usuário do Github</label>
+              <input name="github_username" id="username_github" required />
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="stack">Tecnologias</label>
+              <input name="stack" id="stack" required />
+            </div>
+
+            <div className="input-group">
+              <div className="input-block">
+                <label htmlFor="latitude">Latitude</label>
+                <input name="latitude" id="latitude" required />
+              </div>
+
+              <div className="input-block">
+                <label htmlFor="logitude">Logitude</label>
+                <input name="logitude" id="logitude" required />
+              </div>
+            </div>
+
+            <button type="submit">Salvar</button>
+          </form>
+        </aside>
+
+        <main></main>
+      </div>
+    </>
   );
 }
 
